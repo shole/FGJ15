42246ab864368f8ff503782653a1775db05bee01
shole <pahappo@gmail.com>
2015-01-24 13:29:05 +0200

    player direction wheel UI facing camera script scene adjustments

---
+++ UnityProject/Assets/ScriptsUI/AlwaysFaceGameplayOrigin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlwaysFaceGameplayOrigin : MonoBehaviour
+{
+
+    Transform GameplayOrigin;
+
+	// Use this for initialization
+	void Start () {
+        GameplayOrigin = GameObject.Find("GameplayOrigin").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(GameplayOrigin.position);
+	}
+}
