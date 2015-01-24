eb643d7bc5c8fa85cf869fc741bd03800ece09cc
shole <pahappo@gmail.com>
2015-01-25 01:40:37 +0200

    added directional move & playerid to tentacle

--- UnityProject/Assets/Scripts/CubeMover.cs
+++ UnityProject/Assets/Scripts/CubeMover.cs
@@ -5,11 +5,31 @@ public class CubeMover : MonoBehaviour {
     public TouchInputReader input;
     public float moveForceModifier = 15f;
     public float pullForceModifier = 100f;
-	
+
+    public GameObject playerIndicatorPrefab;
+    GameObject playerIndicator;
+    Transform playerDirection;
+    Transform reachTarget;
+
+    void Start()
+    {
+        playerIndicator = (GameObject)Instantiate(playerIndicatorPrefab);
+        playerIndicator.transform.parent = transform;
+        playerIndicator.transform.localPosition = Vector3.zero;
+        playerDirection = playerIndicator.transform.FindChild("DirectionWheel");
+        reachTarget = playerDirection.FindChild("ReachTarget");
+        Debug.Log("playerid " + input.photonView.owner.ID);
+        playerIndicator.transform.FindChild("playerno").gameObject.GetComponent<TextMesh>().text = "" + input.photonView.owner.ID;
+        playerIndicator.transform.FindChild("playernoshadow").gameObject.GetComponent<TextMesh>().text = "" + input.photonView.owner.ID;
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (input.lastX != 0 || input.lastY != 0)
         {
+
+            playerDirection.localRotation = Quaternion.AngleAxis(Mathf.Atan2(input.lastX, input.lastY) * 60, Vector3.forward);
+
             if (rigidbody.isKinematic)
             {
                 // grabbing is on, and user is giving input, so cause a force
@@ -45,14 +65,16 @@ public class CubeMover : MonoBehaviour {
                 if (endObject != null && beginObject != null)
                 {
                     Vector3 direction = endObject.position - beginObject.position;
-
+                    
                     beginObject.AddForce(direction * pullForceModifier);
                 }
             }
             else
             {
                 // just move the tentacle
-                rigidbody.AddForce(input.lastX * moveForceModifier, 0, input.lastY * moveForceModifier);
+
+                rigidbody.AddForce((reachTarget.position - transform.position).normalized * moveForceModifier);
+                //rigidbody.AddForce(input.lastX * moveForceModifier, 0, input.lastY * moveForceModifier);
             }
         }
         if (input.unhandledDoubleTap)
