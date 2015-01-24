4f99a91c7fb62424a29c571b4388c946e93e9250
jannera <janne.rasanen@vincit.fi>
2015-01-24 17:43:04 +0200

    Android now moves the cube

---
+++ UnityProject/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMover : MonoBehaviour {
+    private TouchInputReader input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (input == null)
+        {
+            input = FindObjectOfType<TouchInputReader>();
+        }
+        if (input == null)
+        {
+            Debug.Log("couldn't find input");
+            return;
+        }
+
+        Debug.Log("translating.. " + input.lastY + ", " + input.lastX);
+        transform.Translate(input.lastX / 100f, 0, input.lastY/100f);
+	}
+}
