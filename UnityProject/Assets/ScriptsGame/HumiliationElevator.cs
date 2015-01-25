ccef010e38533a04ccdfeb9d1edc5b58c82ea8fd
shole <pahappo@gmail.com>
2015-01-25 04:38:14 +0200

    added humiliation elevator

---
+++ UnityProject/Assets/ScriptsGame/HumiliationElevator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumiliationElevator : MonoBehaviour {
+
+    public float elevatorSpeed = 0.1f;
+    public float liftHeight = 10f;
+    public bool isBeingHumiliated = true;
+    public void HumiliatePlayer()
+    {
+        isBeingHumiliated = true;
+        rigidbody.isKinematic = true;
+        state = 0;
+    }
+    int state = 0;
+    Transform droppoint;
+
+	// Use this for initialization
+	void Start () {
+        droppoint = GameObject.Find("OctopusStartingPos").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (isBeingHumiliated)
+        {
+            if (state == 0)
+            {
+                if (transform.position.y <= liftHeight)
+                {
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, droppoint.position.y + liftHeight, transform.position.z), elevatorSpeed);
+                }
+                else
+                {
+                    state = 1;
+                }
+            }
+            if (state==1){
+                if(Vector3.Distance(transform.position,droppoint.position)>0.5f){
+                    transform.position = Vector3.Lerp(transform.position, droppoint.position, elevatorSpeed);
+                } else {
+                    isBeingHumiliated=false;
+                    rigidbody.isKinematic = false;
+                    state = 0;
+                }
+            }
+        }
+	}
+}
---
+++ UnityProject/Assets/ScriptsGame/Playerdeath.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playerdeath : MonoBehaviour {
+
+    public float deathaccelerator=1.1f;
+    Transform octopistartposition;
+
+	// Use this for initialization
+	void Start () {
+        octopistartposition = transform.parent.parent.FindChild("OctopusStartingPos");
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+    void OnTriggerEnter(Collider hit)
+    {
+        GameObject ob = hit.gameObject;
+        if (hit.tag == "Player")
+        {
+            Debug.Log("Shit hit the fan " + hit.name);
+            // if - add check for dead player here and murder prefab if dead!
+            ob.GetComponent<HumiliationElevator>().HumiliatePlayer();
+            /*
+            Rigidbody[] everybody = ob.transform.GetComponentsInChildren<Rigidbody>();
+            for (int i = 0; i < everybody.Length; i++)
+            {
+                everybody[i].Sleep();
+                everybody[i].isKinematic = true;
+
+                everybody[i].transform.position = octopistartposition.position;// +(everybody[i].transform.position - ob.transform.parent.position);
+
+                everybody[i].isKinematic = false;
+
+                everybody[i].velocity = Vector3.zero;
+                everybody[i].angularVelocity = Vector3.zero;
+
+                everybody[i].mass *= deathaccelerator;
+                everybody[i].WakeUp();
+            }
+
+            ob.transform.parent.position = octopistartposition.position;
+
+            ob.transform.rigidbody.Sleep();
+
+            ob.transform.rigidbody.isKinematic = true;
+
+            ob.transform.localScale = hit.transform.localScale * deathaccelerator;
+
+            ob.transform.rigidbody.isKinematic = false;
+            ob.transform.rigidbody.velocity = Vector3.zero;
+            ob.transform.rigidbody.angularVelocity = Vector3.zero;
+            ob.transform.rigidbody.WakeUp();
+
+            // rigidbody mass
+            // strength
+            
+            // set all rigidbody.velocity=Vector3.zero;
+            */
+        }
+    }
+}
