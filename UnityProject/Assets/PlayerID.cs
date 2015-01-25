14092eed5a68b00a1238e3c911e915af675ce33d
jannera <janne.rasanen@vincit.fi>
2015-01-25 18:05:54 +0200

    Added player id to android device

---
+++ UnityProject/Assets/PlayerID.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerID : PhotonBehaviour {
+    Text text;
+    
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PhotonNetwork.player == null)
+        {
+            return;
+        }
+        if (PhotonNetwork.player.ID == null)
+        {
+            Debug.Log("player id null");
+            return;
+        }
+        text.text = PhotonNetwork.player.ID.ToString();
+	}
+}
