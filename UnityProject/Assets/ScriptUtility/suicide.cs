4fd2202b03cd915ddf50cee2be6d231e40707bbd
shole <pahappo@gmail.com>
2015-01-25 13:26:27 +0200

    implemented fancy bubble prefabs

---
+++ UnityProject/Assets/ScriptUtility/suicide.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class suicide : MonoBehaviour {
+
+    public float timeToDie = 10f;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, timeToDie);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
