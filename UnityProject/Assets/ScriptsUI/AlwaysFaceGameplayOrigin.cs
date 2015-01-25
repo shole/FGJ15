6413d94b229d86616b5f52759e22a6944f92be9e
shole <pahappo@gmail.com>
2015-01-25 13:01:45 +0200

    made reach direction body centric
    updated octipi texture
    added bubbles prefab

--- UnityProject/Assets/ScriptsUI/AlwaysFaceGameplayOrigin.cs
+++ UnityProject/Assets/ScriptsUI/AlwaysFaceGameplayOrigin.cs
@@ -6,13 +6,26 @@ public class AlwaysFaceGameplayOrigin : MonoBehaviour
 
     Transform GameplayOrigin;
 
+    Transform parentbody;
+
 	// Use this for initialization
 	void Start () {
         GameplayOrigin = GameObject.Find("GameplayOrigin").transform;
+        parentbody = transform.parent;
+        while (parentbody.gameObject.layer == LayerMask.NameToLayer("Tentacle"))
+        {
+            parentbody = parentbody.parent;
+        }
+        parentbody=parentbody.FindChild("Armature|Base");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(GameplayOrigin.position);
+        //Vector3 pos = transform.position;
+        //transform.rotation = Quaternion.LookRotation((GameplayOrigin.position - transform.position).normalized);
+        transform.rotation = Quaternion.LookRotation((GameplayOrigin.position - parentbody.position).normalized);
+        
+        transform.rotation.SetAxisAngle(Vector3.right, 22f);
+        //transform.LookAt(GameplayOrigin.position);
 	}
 }
